f8a1f3f80aa4972c2e04c8d47eb133a19b6f2cec
krafik <alexeym4l@mail.ru>
2023-08-14 20:31:48 +0300

    добавил пару карточек

--- src/App.vue
+++ src/App.vue
@@ -3,7 +3,7 @@
     <div class="title has-text-centered">
       my todo list
     </div>
-    <div class="field is-grouped">
+    <div class="field is-grouped mb-5 ">
       <p class="control is-expanded">
         <input class="input" type="text" placeholder="add todo">
       </p>
@@ -12,6 +12,23 @@
           add
         </a>
       </p>
+    </div>
+    <div
+    v-for="i in 3" :key="i"
+    class="card mb-5">
+      <div class="card-content">
+        <div class="content">
+          <div class="columns is-mobile is-vcentered">
+            <div class="column"> hello</div>
+            <div class="column is-5 has-text-right">
+              <button class="button is-ligth">&check;</button>
+              <button class="button is-danger ml-2">&cross;</button>
+            </div>
+          </div>
+
+
+        </div>
+      </div>
     </div>
   </div>
 </template>
