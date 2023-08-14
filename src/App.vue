3535bd0f72186888bedffe9fe21fe6aedd10821b
krafik <alexeym4l@mail.ru>
2023-08-14 20:07:05 +0300

    добавил базовую разметку тудулиста

--- src/App.vue
+++ src/App.vue
@@ -1,4 +1,29 @@
 <template>
-<h1>hello</h1>  
+  <div class="wrapper-todo">
+    <div class="title has-text-centered">
+      my todo list
+    </div>
+    <div class="field is-grouped">
+      <p class="control is-expanded">
+        <input class="input" type="text" placeholder="add todo">
+      </p>
+      <p class="control">
+        <a class="button is-info">
+          add
+        </a>
+      </p>
+    </div>
+  </div>
 </template>
+
+
+<style>
+@import "/node_modules/bulma/css/bulma.min.css";
+
+.wrapper-todo {
+  max-width: 400px;
+  padding: 20px;
+  margin: 0 auto;
+}
+</style>
 
