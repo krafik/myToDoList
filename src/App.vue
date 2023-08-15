35af66db27214bb8ae5239bfdd47799414100d91
krafik <alexeym4l@mail.ru>
2023-08-15 23:36:06 +0300

    добавил функционал toggler-a

--- src/App.vue
+++ src/App.vue
@@ -16,13 +16,14 @@
       </div>
     </form>
 
-    <div v-for="todo in todos" :key="todo.id" class="card mb-5">
+    <div :class="{ 'has-background-success-light': todo.done }" v-for="todo in todos" :key="todo.id" class="card mb-5">
       <div class="card-content">
         <div class="content">
           <div class="columns is-mobile is-vcentered">
-            <div class="column"> {{ todo.content }}</div>
+            <div class="column" :class="{ 'has-text-success line-through': todo.done }"> {{ todo.content }}</div>
             <div class="column is-5 has-text-right">
-              <button class="button is-ligth">&check;</button>
+              <button @click="togglerDone(todo.id)" class="button is-ligth"
+                :class="todo.done ? 'is-success' : 'is-light'">&check;</button>
               <button @click="deleteToDo(todo.id)" class="button is-danger ml-2">&cross;</button>
             </div>
           </div>
@@ -42,16 +43,16 @@ import { v4 as uuidv4 } from "uuid";
 //todo
 
 const todos = ref([
-  {
-    id: 'id1',
-    content: 'some',
-    done: false,
-  },
-  {
-    id: 'id2',
-    content: 'some',
-    done: false,
-  }
+  // {
+  //   id: 'id1',
+  //   content: 'some',
+  //   done: false,
+  // },
+  // {
+  //   id: 'id2',
+  //   content: 'some',
+  //   done: true,
+  // }
 ]);
 
 //methods
@@ -71,9 +72,16 @@ const addToDo = () => {
 }
 
 //delete todo
-const deleteToDo = id =>{
+const deleteToDo = id => {
   console.log('deleteid', id);
-  todos.value = todos.value.filter(todo=>todo.id !== id)
+  todos.value = todos.value.filter(todo => todo.id !== id)
+}
+
+//togglerDone
+const togglerDone = id => {
+  const index = todos.value.findIndex(todo=> todo.id === id);
+  console.log(index);
+  todos.value[index].done = !todos.value[index].done
 }
 </script>
 
@@ -86,6 +94,10 @@ const deleteToDo = id =>{
   max-width: 400px;
   padding: 20px;
   margin: 0 auto;
+}
+
+.line-through {
+  text-decoration: line-through;
 }
 </style>
 
